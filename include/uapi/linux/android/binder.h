3775d47229adce33cab75705bf5ea025cd5f5894
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-08-18 11:48:01 -0400

    Revert "ANDROID: binder: remove 32-bit binder interface."
    This reverts commit 1190b4e38f97023154e6b3bef61b251aa5f970d0.
    
    http://github.com/aosp-mirror/kernel_common/commit/1190b4e38f97023154e6b3bef61b251aa5f970d0.diff
    
    Signed-off-by: Christopher Lentocha <christopherericlentocha@gmail.com>

--- include/uapi/linux/android/binder.h
+++ include/uapi/linux/android/binder.h
@@ -99,7 +99,7 @@ enum flat_binder_object_flags {
 	FLAT_BINDER_FLAG_TXN_SECURITY_CTX = 0x1000,
 };
 
-#ifdef BINDER_IPC_32BIT
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 typedef __u32 binder_size_t;
 typedef __u32 binder_uintptr_t;
 #else
@@ -237,7 +237,7 @@ struct binder_version {
 };
 
 /* This is the current protocol version. */
-#ifdef BINDER_IPC_32BIT
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 #define BINDER_CURRENT_PROTOCOL_VERSION 7
 #else
 #define BINDER_CURRENT_PROTOCOL_VERSION 8
